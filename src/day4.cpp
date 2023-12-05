946ae148d82ce4ce6d1134246a805f29b3f5d575
bwpge <bwpge.dev@gmail.com>
2023-12-04 22:17:31 -0500

    Add day 4 part 2 solution

--- src/day4.cpp
+++ src/day4.cpp
@@ -1,6 +1,8 @@
 #include "aoc/aoc.hpp"
 
 #include <iostream>
+#include <map>
+#include <ranges>
 #include <string>
 #include <unordered_set>
 #include <vector>
@@ -41,19 +43,24 @@ public:
     }
 
     [[nodiscard]]
-    size_t points() const {
-        int matches{};
+    int matches() const {
+        int count{};
         for (const auto& num : _numbers) {
             if (_winning.contains(num)) {
-                matches++;
+                count++;
             }
         }
+        return count;
+    }
 
-        if (matches <= 0) {
+    [[nodiscard]]
+    size_t points() const {
+        auto count = static_cast<size_t>(matches());
+        if (count <= 0) {
             return 0;
         }
 
-        return 1ULL << (matches - 1);
+        return 1ULL << (count - 1);
     }
 
 private:
@@ -62,19 +69,64 @@ private:
     std::vector<std::string> _numbers{};
 };
 
-void part1() {
-    auto input = aoc::open("data/day4.txt");
+std::vector<Card> parse_cards(const std::filesystem::path& path) {
+    auto input = aoc::open(path);
 
-    size_t sum{};
+    std::vector<Card> cards{};
     std::string line{};
     while (std::getline(input, line)) {
-        auto card = Card::parse(line);
+        cards.emplace_back(Card::parse(line));
+    }
+
+    return cards;
+}
+
+void part1(const std::vector<Card>& cards) {
+    size_t sum{};
+    for (const auto& card : cards) {
         sum += card.points();
     }
 
-    std::cout << "Result: " << sum << '\n';
+    std::cout << "Part 1: " << sum << '\n';
+}
+
+void part2(const std::vector<Card>& cards) {
+    std::map<int, int> counts{};
+
+    auto inc_count = [&counts](int id, int amount = 1) {
+        if (!counts.contains(id)) {
+            counts[id] = amount;
+        } else {
+            counts[id] += amount;
+        }
+    };
+
+    for (const auto& card : cards) {
+        // this is the "original" card, so we need to track it in the map.
+        // no matter what, the count always increments by 1 since this is
+        // the first time we're seeing it
+        auto id = card.id();
+        inc_count(id);
+
+        auto start = card.id() + 1;
+        auto last = start + static_cast<int>(card.matches());
+        auto copies = counts[id];
+
+        // generate sequential copies for each copy of the current card
+        for (auto i : std::views::iota(start, last)) {
+            inc_count(i, copies);
+        }
+    }
+
+    int sum{};
+    for (const auto& [_, count] : counts) {
+        sum += count;
+    }
+    std::cout << "Part 2: " << sum << '\n';
 }
 
 int main() noexcept(false) {
-    part1();
+    auto cards = parse_cards("data/day4.txt");
+    part1(cards);
+    part2(cards);
 }
