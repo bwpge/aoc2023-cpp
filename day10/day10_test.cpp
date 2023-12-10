c7c1817b620772c3f5374be708055f2b8dd2f72e
bwpge <bwpge.dev@gmail.com>
2023-12-10 15:09:13 -0500

    Implement maze infrastructure

--- day10/day10_test.cpp
+++ day10/day10_test.cpp
@@ -2,6 +2,106 @@
 
 #include <gtest/gtest.h>
 
-TEST(Day10, HelloWorld) {
-    ASSERT_TRUE(true);
+using day10::Maze;
+using Direction = Maze::Direction;
+using Coord = Maze::Coord;
+
+const std::vector<std::string> SIMPLE_PIPES{
+    ".....",
+    ".S-7.",
+    ".|.|.",
+    ".L-J.",
+    ".....",
+};
+
+const std::vector<std::string> PIPES{
+    "-L|F7",
+    "7S-7|",
+    "L|7||",
+    "-L-J|",
+    "L|-JF",
+};
+
+TEST(Day10, Maze) {
+    auto m = Maze::parse(SIMPLE_PIPES);
+
+    EXPECT_EQ(m.width(), 5);
+    EXPECT_EQ(m.height(), 5);
+}
+
+TEST(Day10, MazeConnects) {
+    // has the form: [ coord, expected=(north, east, south, west) ]
+    const std::vector<std::pair<Coord, std::tuple<bool, bool, bool, bool>>> test_data{
+        {{0, 0}, {false, false, false, false}},  // .
+        {{1, 1}, {true, true, true, true}},  // S
+        {{2, 1}, {false, true, false, true}},  // -
+        {{3, 1}, {false, false, true, true}},  // 7
+        {{1, 2}, {true, false, true, false}},  // |
+        {{3, 2}, {true, false, true, false}},  // |
+        {{1, 3}, {true, true, false, false}},  // L
+        {{2, 3}, {false, true, false, true}},  // -
+        {{3, 3}, {true, false, false, true}},  // J
+    };
+
+    auto m = Maze::parse(SIMPLE_PIPES);
+    for (const auto& [pos, expected] : test_data) {
+        auto [expect_n, expect_e, expect_s, expect_w] = expected;
+        auto msg = fmt::format("at pos=({}, {})", pos.first, pos.second);
+
+        EXPECT_EQ(m.connects_to(pos, Direction::North), expect_n) << msg;
+        EXPECT_EQ(m.connects_to(pos, Direction::East), expect_e) << msg;
+        EXPECT_EQ(m.connects_to(pos, Direction::South), expect_s) << msg;
+        EXPECT_EQ(m.connects_to(pos, Direction::West), expect_w) << msg;
+    }
+}
+
+TEST(Day10, MazeConnectsTo) {
+    auto m = Maze::parse(PIPES);
+
+    // test incoming connections to start
+    Coord start{1, 1};
+    Coord north{1, 0};
+    Coord east{2, 1};
+    Coord south{1, 2};
+    Coord west{0, 1};
+    EXPECT_FALSE(m.connects_to(north, start));
+    EXPECT_TRUE(m.connects_to(east, start));
+    EXPECT_TRUE(m.connects_to(south, start));
+    EXPECT_FALSE(m.connects_to(west, start));
+
+    // test some arbitrary tiles
+    EXPECT_FALSE(m.connects_to({0, 0}, {0, 1}));  // - to 7, S
+    EXPECT_FALSE(m.connects_to({2, 4}, {2, 3}));  // - to -, N
+    EXPECT_TRUE(m.connects_to({3, 3}, {2, 3}));  // J to -, W
+    EXPECT_TRUE(m.connects_to({1, 3}, {2, 3}));  // L to -, E
+}
+
+TEST(Day10, MazeAdjacent) {
+    auto m = Maze::parse(PIPES);
+    std::vector<std::pair<Coord, std::vector<Coord>>> expect{
+        {
+            {1, 1},
+            {{1, 2}, {2, 1}},
+        },
+        {{2, 1}, {{1, 1}, {3, 1}}},
+        {{2, 2}, {}},
+        {{3, 3}, {{3, 2}, {2, 3}}}
+    };
+
+    for (const auto& [src, coords] : expect) {
+        auto adjacent = m.adjacent(src);
+        auto msg_size = fmt::format("size mistmatch for src=({}, {})", src.first, src.second);
+        EXPECT_EQ(adjacent.size(), coords.size()) << msg_size;
+
+        for (const auto& expected : coords) {
+            auto msg = fmt::format(
+                "missing expected neighbor ({}, {}) for src=({}, {})",
+                expected.first,
+                expected.second,
+                src.first,
+                src.second
+            );
+            EXPECT_TRUE(aoc::contains(adjacent, expected)) << msg;
+        }
+    }
 }
--- day10/include/day10.hpp
+++ day10/include/day10.hpp
@@ -2,4 +2,176 @@
 
 #include "aoc/aoc.hpp"
 
-namespace day10 {}  // namespace day10
+namespace day10 {
+
+class Maze {
+public:
+    using Coord = std::pair<size_t, size_t>;
+
+    enum class Direction {
+        North,
+        East,
+        South,
+        West
+    };
+
+    static Direction direction(Coord from, Coord to) {
+        auto dx = static_cast<int64_t>(to.first) - static_cast<int64_t>(from.first);
+        auto dy = static_cast<int64_t>(to.second) - static_cast<int64_t>(from.second);
+
+        AOC_ASSERT(
+            (std::abs(dx) + std::abs(dy)) == 1,
+            "coordinates must be adjacent in a cardinal direction"
+        );
+
+        if (dy > 0) {
+            return Direction::South;
+        }
+        if (dy < 0) {
+            return Direction::North;
+        }
+        if (dx > 0) {
+            return Direction::East;
+        }
+        if (dx < 0) {
+            return Direction::West;
+        }
+
+        aoc::unreachable();
+    }
+
+    static Maze parse(const std::vector<std::string>& lines) {
+        AOC_ASSERT(!lines.empty(), "lines must not be empty");
+
+        Maze result{};
+        for (const auto& line : lines) {
+            result._grid.emplace_back(line.begin(), line.end());
+        }
+
+        // verify dimensions
+        size_t width{result._grid.front().size()};
+        for (const auto& row : result._grid) {
+            AOC_ASSERT(row.size() == width, "all rows must have equal size");
+        }
+
+        // store start coordinate
+        size_t y{};
+        bool found = false;
+        for (const auto& row : result._grid) {
+            if (!aoc::contains(row, 'S')) {
+                ++y;
+                continue;
+            }
+
+            for (size_t x = 0; x < row.size(); ++x) {
+                if (row[x] == 'S') {
+                    result._start = {x, y};
+                    found = true;
+                    break;
+                }
+            }
+            break;
+        }
+
+        return result;
+    }
+
+    [[nodiscard]]
+    bool connects_to(Coord pos, Direction dir) const {
+        auto c = at(pos);
+
+        // starting tile has to infer from other connections
+        if (c == 'S') {
+            return true;
+        }
+
+        switch (dir) {
+            case Direction::North:
+                return c == '|' || c == 'L' || c == 'J';
+            case Direction::East:
+                return c == '-' || c == 'L' || c == 'F';
+            case Direction::South:
+                return c == '|' || c == '7' || c == 'F';
+            case Direction::West:
+                return c == '-' || c == 'J' || c == '7';
+            default:
+                return false;
+        }
+    }
+
+    [[nodiscard]]
+    bool connects_to(Coord src, Coord dst) const {
+        auto dir = Maze::direction(src, dst);
+        return connects_to(src, dir);
+    }
+
+    [[nodiscard]]
+    bool is_adjacent(Coord src, Coord dst) const {
+        // NOLINTNEXTLINE(readability-suspicious-call-argument)
+        return connects_to(src, dst) && connects_to(dst, src);
+    }
+
+    [[nodiscard]]
+    std::vector<Coord> adjacent(Coord pos) {
+        std::vector<Coord> result{};
+
+        auto push_coord = [this, &result](const Coord& src, const Coord& dst) {
+            if (is_adjacent(src, dst)) {
+                result.push_back(dst);
+            }
+        };
+
+        // north
+        if (pos.second > 0) {
+            push_coord(pos, {pos.first, pos.second - 1});
+        }
+        // east
+        if (pos.first + 1 <= width()) {
+            push_coord(pos, {pos.first + 1, pos.second});
+        }
+        // south
+        if (pos.second + 1 <= height()) {
+            push_coord(pos, {pos.first, pos.second + 1});
+        }
+        // west
+        if (pos.first > 0) {
+            push_coord(pos, {pos.first - 1, pos.second});
+        }
+
+        return result;
+    }
+
+    [[nodiscard]]
+    char at(Coord pos) const {
+        return _grid[pos.second][pos.first];  // use row major
+    }
+
+    [[nodiscard]]
+    char at(size_t x, size_t y) const {
+        return _grid[y][x];  // use row major
+    }
+
+    void set_tile(Coord pos, char c) {
+        _grid[pos.second][pos.first] = c;
+    }
+
+    void set_tile(size_t x, size_t y, char c) {
+        _grid[y][x] = c;
+    }
+
+    [[nodiscard]]
+    size_t width() const {
+        return _grid.front().size();
+    }
+
+    [[nodiscard]]
+    size_t height() const {
+        return _grid.size();
+    }
+
+private:
+    std::vector<std::vector<char>> _grid{};
+    Coord _start{};
+};
+
+}  // namespace day10
