2b72a5c02ac3c3ff3c398f2712ea2f08397ae131
bwpge <bwpge.dev@gmail.com>
2023-12-11 14:58:14 -0500

    Add type aliases

--- libaoc/include/aoc/aoc.hpp
+++ libaoc/include/aoc/aoc.hpp
@@ -1,8 +1,9 @@
 #pragma once
 
+#include "aoc/types.hpp"  // IWYU pragma: export
+
 #include "aoc/collections.hpp"  // IWYU pragma: export
 #include "aoc/core.hpp"  // IWYU pragma: export
 #include "aoc/digit.hpp"  // IWYU pragma: export
 #include "aoc/fs.hpp"  // IWYU pragma: export
 #include "aoc/stringutils.hpp"  // IWYU pragma: export
-#include "aoc/types.hpp"  // IWYU pragma: export
--- libaoc/include/aoc/types.hpp
+++ libaoc/include/aoc/types.hpp
@@ -1,8 +1,24 @@
 #pragma once
 
+#include <cstddef>
+#include <cstdint>
 #include <string_view>
 
 namespace aoc {
+
+using u8 = std::uint8_t;
+using u16 = std::uint16_t;
+using u32 = std::uint32_t;
+using u64 = std::uint64_t;
+using usize = std::size_t;
+
+using i8 = std::int8_t;
+using i16 = std::int16_t;
+using i32 = std::int32_t;
+using i64 = std::int64_t;
+
+using f32 = float;
+using f64 = double;
 
 template<typename T>
 concept CharLike = std::convertible_to<T, char>;
